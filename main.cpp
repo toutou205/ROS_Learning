7c07bb4f00971394627e203b0dbc53f4edd9ffab
toutou205 <toutou205@qq.com>
2021-01-21 16:05:09 +0800

    another version

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
  
-int main()
-{
-    cout << "hello" << endl;
-    return 0;
+// int main()
+// {
+//     cout << "hello" << endl;
+//     return 0;
+// }
+
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main() {
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code!"};
+    for (const string& word : msg) {
+        cout << word << " ";
+    }
+    cout << endl;
 }
